4b2178460cdb42c6e8cd888f8ac9306f498fbdda
Peter Jang <peter.x.jang@gmail.com>
2018-12-02 10:31:28 -0600

    Add pages show action

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -2,4 +2,8 @@ class Api::PagesController < ApplicationController
   def index
     render json: {message: "Hello Peter!, heyy, it's daniel s"}
   end
+
+  def show
+    render json: {message: "Here's info about id #{params[:id]}"}
+  end
 end
